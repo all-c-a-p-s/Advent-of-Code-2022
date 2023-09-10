2cc4eed8c1b117399fe94b86f4824ecbdb9e2e62
Sebastiano Rebonato-Scott <141144328+all-caps-when-you-spell-the-user-name@users.noreply.github.com>
2023-09-10 17:42:50 +0100

    day 6 alternate solution

--- Avent of Code/day06/hashset_alternate_solution.go
+++ Avent of Code/day06/hashset_alternate_solution.go
@@ -1,10 +1,9 @@
-//More concise code using hashmap to measure unique elements, but this is ~6 times slower (i think because of heap allocation to hashmap)
+//roughly 1.8x slower than that just loops through string window, but more concise
 package main
 
 import (
 	"fmt"
 	"os"
-	"time"
 )
 
 func check(e error){
@@ -22,8 +21,11 @@ func read_file() string {
 func find_marker(signal string) int {
 
 	for i := 14;i < len(signal);i++{
-		hashSet := map[byte]int{} //data types don't really matter
+		hashSet := map[byte]int{} //second data type doesn't really matter
 		for j := 0;j < 14;j++{
+			if _, ok := hashSet[signal[i - 14 + j]]; ok{
+				break
+			}
 			hashSet[signal[i - 14 + j]] = j
 		}
 		if len(hashSet) == 14{
@@ -34,9 +36,6 @@ func find_marker(signal string) int {
 }
 
 func main(){
-	start := time.Now()
 	input_string := read_file()
 	fmt.Println(find_marker(input_string))
-	duration := time.Since(start)
-	fmt.Println(duration)
 }
